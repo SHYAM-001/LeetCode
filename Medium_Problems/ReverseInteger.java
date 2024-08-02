d59595f299c8200c9e90a9f7e5334af82451b68d
SHYAM-001 <shyamkathir007@gmail.com>
2024-08-02 22:22:43 +0530

    ReverseInteger problem solution is added

--- Medium_Problems/ReverseInteger.java
+++ Medium_Problems/ReverseInteger.java
@@ -1,9 +1,21 @@
 import java.util.*;
 
-public class ReverseInteger
-{
-    public static void main(String[] args)
-    {
-        
+public class ReverseInteger {
+    public static int reverse(int x) {
+        int num = Math.abs(x);
+        int result = 0;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            result = result * 10 + lastDigit;
+            num = num / 10;
+        }
+        return (x < 0) ? -result : result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = reverse(n);
+        System.out.println(result);
     }
 }
