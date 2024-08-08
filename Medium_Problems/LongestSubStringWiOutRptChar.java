789fec8d83b3c7bfdf85bba69447172d15375d92
SHYAM-001 <shyamkathir007@gmail.com>
2024-08-08 13:32:23 +0530

    updated the medium problem solution and updated the readme

--- Medium_Problems/LongestSubStringWiOutRptChar.java
+++ Medium_Problems/LongestSubStringWiOutRptChar.java
@@ -1,7 +1,29 @@
 import java.util.*;
 
 public class LongestSubStringWiOutRptChar {
-    public static int lengthOfLongestSubstring(String s) {
+
+    // SetMethod
+    public static int SetMethod(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0;
+        int maxlength = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                maxlength = Math.max(maxlength, right - left + 1);
+            } else {
+                while (set.contains(s.charAt(right))) {
+                    set.remove(s.charAt(left));
+                    left++;
+                }
+                set.add(s.charAt(right));
+            }
+        }
+        return maxlength;
+    }
+
+    // HashMapMethod
+    public static int HashMapMethod(String s) {
         HashMap<Character, Integer> m = new HashMap<>();
         int left = 0;
         int maxlength = 0;
@@ -18,9 +40,27 @@ public class LongestSubStringWiOutRptChar {
         return maxlength;
     }
 
+    // Integer Array Method
+    public static int IntegerArrayMethod(String s) {
+        int n = s.length();
+        int maxLength = 0;
+        int[] charIndex = new int[128]; // ASCII character set
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+            if (charIndex[s.charAt(right)] > left) {
+                left = charIndex[s.charAt(right)];
+            }
+            charIndex[s.charAt(right)] = right + 1;
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        System.out.println(lengthOfLongestSubstring(input));
+        System.out.println(HashMapMethod(input));
     }
 }
